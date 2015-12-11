ea20b61ce0013b2b806e61f8028670d5620b1932
Yevhen Herasymenko <fess2036@ya.ru>
2015-12-11 21:52:56 +0200

    feat: get request weather for city

--- FirstGroup/FirstGroup/ViewController.m
+++ FirstGroup/FirstGroup/ViewController.m
@@ -8,7 +8,14 @@
 
 #import "ViewController.h"
 
-@interface ViewController ()
+static NSString *const BasicUrl = @"http://api.openweathermap.org";
+static NSString *const WeatherCityNameUrl = @"/data/2.5/weather?q=%@,uk&appid=2de143494c0b295cca9337e1e96b00e0&mine=true";
+
+@interface ViewController ()<UITextFieldDelegate>
+@property (weak, nonatomic) IBOutlet UIView *temperatureBackground;
+@property (weak, nonatomic) IBOutlet UILabel *temperatureLabel;
+@property (weak, nonatomic) IBOutlet UILabel *cityNameLabel;
+
 
 @end
 
@@ -16,12 +23,43 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    self.temperatureBackground.layer.masksToBounds = YES;
+    self.temperatureBackground.layer.cornerRadius = CGRectGetWidth(self.temperatureBackground.frame)/2;
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (void)configurationScreenWithDictionary:(NSDictionary *)weatherDictionary {
+    dispatch_async(dispatch_get_main_queue(), ^{
+        NSString *nameCity = weatherDictionary[@"name"];
+        self.cityNameLabel.text = nameCity;
+        NSDictionary *mainDictionary = weatherDictionary[@"main"];
+        NSString *temperatureString = mainDictionary[@"temp"];
+        NSInteger temp = [temperatureString integerValue];
+        self.temperatureLabel.text = [@(temp - 273) description];
+    });
+
+}
+
+- (BOOL)textFieldShouldReturn:(UITextField *)textField {
+    [textField resignFirstResponder];
+    __weak typeof(self) weakSelf = self;
+    
+    //session
+    NSURLSession *session = [NSURLSession sharedSession];
+    
+    NSString *urlString = [BasicUrl stringByAppendingString:WeatherCityNameUrl];
+    urlString = [NSString stringWithFormat:urlString, textField.text];
+    NSURL *url = [NSURL URLWithString:urlString];
+    
+    
+    NSURLSessionDataTask *dataTask = [session dataTaskWithURL:url
+                                            completionHandler:^(NSData *data, NSURLResponse *response, NSError *error) {
+                                                __strong typeof(weakSelf) strongSelf = weakSelf;
+                                                NSDictionary *json = [NSJSONSerialization JSONObjectWithData:data options:0 error:nil];
+                                                [strongSelf configurationScreenWithDictionary:json];
+                                            }];
+    [dataTask resume];
+    return YES;
 }
 
 @end
