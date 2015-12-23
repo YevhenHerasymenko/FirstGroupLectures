9f913f6530be9185a1c1ef0e43ef138d39ec1b13
Yevhen Herasymenko <fess2036@ya.ru>
2015-12-23 20:46:57 +0200

    feat: CoreData

---
+++ FirstGroup/FirstGroup/ViewControllers/FGPFacebookViewController.h
@@ -0,0 +1,13 @@
+//
+//  FGPFacebookViewController.h
+//  FirstGroup
+//
+//  Created by Yevhen Herasymenko on 18/12/2015.
+//  Copyright © 2015 YevhenHerasymenko. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FGPFacebookViewController : UIViewController
+
+@end
---
+++ FirstGroup/FirstGroup/ViewControllers/FGPFindWeatherViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  FirstGroup
+//
+//  Created by Yevhen Herasymenko on 05/12/2015.
+//  Copyright © 2015 YevhenHerasymenko. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FGPFindWeatherViewController : UIViewController
+
+
+@end
+
