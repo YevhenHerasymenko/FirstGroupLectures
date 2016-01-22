44e7a4469568d35a56d59052b06443eb2e19a9eb
Yevhen Herasymenko <fess2036@ya.ru>
2016-01-22 19:25:37 +0200

    feat: create tmdb app

---
+++ TMDB App/TMDB App/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TMDB App
+//
+//  Created by Yevhen Herasymenko on 22/01/2016.
+//  Copyright © 2016 Yevhen Herasymenko. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TMDB App/TMDB App/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TMDB App
+//
+//  Created by Yevhen Herasymenko on 22/01/2016.
+//  Copyright © 2016 Yevhen Herasymenko. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
